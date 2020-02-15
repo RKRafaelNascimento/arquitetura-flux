1bbf5efe67b9712af48b86237d415c7c816e28f1
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-02-15 12:25:20 -0300

    Adicionado configuraçao de routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import Routes from './routes';
 
-function App () {
+function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
-    )
+        <BrowserRouter>
+            {/** <Header/> */}
+            <Routes />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
