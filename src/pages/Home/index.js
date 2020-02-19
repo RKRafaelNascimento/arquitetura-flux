3b13238e444b85e3c6bdbed5f455674ab7c14676
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-02-19 19:17:48 -0300

    Adicionado reducer de carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
 import { ProductList } from './style';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -18,6 +19,14 @@ export default class Home extends Component {
         this.setState({ products: data });
     }
 
+    handlerAddCart = product => {
+        const { dispatch } = this.props;
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
 
@@ -29,7 +38,10 @@ export default class Home extends Component {
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
 
-                        <button type="button">
+                        <button
+                            type="button"
+                            onClick={() => this.handlerAddCart(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" /> 3
                             </div>
@@ -41,3 +53,5 @@ export default class Home extends Component {
         );
     }
 }
+
+export default connect()(Home);
