11d79b1a14a1a56d2a56ef9ab5cc14978796bc5f
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-03-28 15:22:42 -0300

    Adicionado history

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './config/ReactotronConfig';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import Header from './components/Header';
+import history from './services/history';
 import GlobalStyle from './styles/global';
 import store from './store/index';
 
@@ -12,12 +13,12 @@ import Routes from './routes';
 function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <Router history={history}>
                 <Header />
                 <Routes />
                 <GlobalStyle />
                 <ToastContainer autoClose={3000} />
-            </BrowserRouter>
+            </Router>
         </Provider>
     );
 }
